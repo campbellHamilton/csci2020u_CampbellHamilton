ef2aeb2556b9af15aed13a4d3b6d23e161ec814d
CampbellHamilton <campbellHamilton1@uoit.net>
2018-01-19 14:17:39 -0500

    Modified from seperate repo on master branch

--- lab01/HelloWorld.java
+++ lab01/HelloWorld.java
@@ -2,6 +2,7 @@ public class void HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("This is your teammate its totally not just the same person on a copy of the repo");
+        System.out.println("New line on master branch");
 	}
 
 }
